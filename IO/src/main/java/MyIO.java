e939cad94da26d1d447ca80aa7464ae9f5fab5ed
Svetlana <svetlana.tumilevich@gmail.com>
2021-04-25 16:19:04 +0300

    add all calculations

--- IO/src/main/java/MyIO.java
+++ IO/src/main/java/MyIO.java
@@ -1,14 +1,15 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyIO {
 
-    public static List <String> getFilesRecurse(File dir){
-        List <String> listPath = new ArrayList<>();
+    public static List<String> getFilesRecurse(File dir) {
+        List<String> listPath = new ArrayList<>();
         if (dir.isDirectory())
             for (File file : dir.listFiles()) {
-                if(file.isDirectory()) {
+                if (file.isDirectory()) {
                     listPath.add("|-----" + file.getName());
                     listPath.addAll(getFilesRecurse(file));
                 } else {
@@ -20,28 +21,56 @@ public class MyIO {
         return listPath;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("data\\file.txt");
-        for(String element : args) {
+        for (String element : args) {
             System.out.println("My directory - " + element);
             File dir = new File(element);
-            List <String> stringList = getFilesRecurse(dir);
+            List<String> stringList = getFilesRecurse(dir);
 
             try (FileWriter writer = new FileWriter(file, true)) {
                 writer.write(dir.getName());
                 writer.append('\n');
-            for (String elem : stringList) {
+                for (String elem : stringList) {
                     writer.write(elem);
                     writer.append('\n');
                 }
-                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        FileReader reader = new FileReader(file);
-        BufferedReader bufferedReader = new BufferedReader(reader);
-        bufferedReader.lines().forEach(System.out::println);
+        FileReader reader = null;
+        try {
+            reader = new FileReader(file);
 
-
-        bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+            List<String> listOfData = new BufferedReader(reader).lines().collect(Collectors.toList());
+            listOfData.forEach(System.out::println);
+
+            int counterFile = 0;
+            int counterDir = 0;
+            int sumLengthFiles = 0;
+            for (String line : listOfData) {
+                if (line.contains("-----")) {
+                    counterDir++;
+                }
+                if (line.contains("|     ")) {
+                    counterFile++;
+                    sumLengthFiles += line.length();
+                }
+            }
+        if (counterFile == 0) {
+            throw new ArithmeticException("No files in this directory");
+        }
+        if (counterDir == 0) {
+            throw new ArithmeticException("No folders in this directory");
+        }
+        int middleLengthFile = sumLengthFiles / counterFile;
+        int middleQuantityFiles = counterFile / counterDir;
+        System.out.println("Quantity of folders " + counterDir + "\n" + "Quantity of files " + counterFile);
+        System.out.println("Middle length name of file " + middleLengthFile);
+        System.out.println("Middle quantity of files in directory " + middleQuantityFiles);
+    }
 }
