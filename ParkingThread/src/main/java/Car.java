1e87f5fc8088b11453c90a93334fd615df5075b3
Svetlana <svetlana.tumilevich@gmail.com>
2021-05-06 10:52:04 +0300

    correction code

--- ParkingThread/src/main/java/Car.java
+++ ParkingThread/src/main/java/Car.java
@@ -13,16 +13,14 @@ public class Car extends Thread {
     @Override
     public void run() {
         try {
-            if (semaphore.availablePermits() == 0) {
-                System.out.println("Car " + id + " waiting for a free parking space");
-                TimeUnit.SECONDS.sleep(20);
-                System.out.println("Car " + id + " left for another parking lot");
-            } else {
-                semaphore.acquire();
+            System.out.println("Car " + id + " waiting for a free parking space");
+            if (semaphore.tryAcquire(10, TimeUnit.SECONDS)) {
                 System.out.println("Car " + id + " pulls into a parking space");
-                TimeUnit.SECONDS.sleep(30);
-                System.out.println("Car " + id + " frees up parking space");
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println("Car " + id + "frees up parking space");
                 semaphore.release();
+            } else {
+                System.out.println("Car " + id + " left for another parking lot. No parking place");
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
